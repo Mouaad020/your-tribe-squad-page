d7a592ec222b8cc2773e53f863c1b5506fa038ec
Mouaad020 <mouaad.chakroun@hva.nl>
2025-09-16 12:20:44 +0200

    Responsive

--- scripts/squadpagina.js
+++ scripts/squadpagina.js
@@ -1,6 +1,13 @@
 const text = document.querySelector("h3")
 
-text.addEventListener("click", function(){
+text?.addEventListener("click", function(){
 window.location.href = "squadpagina.html"
+console.log("Gelukt");
+})
+
+const home = document.querySelector("h1")
+
+home.addEventListener("click", function(){
+window.location.href = "index.html"
 console.log("Gelukt");
 })
